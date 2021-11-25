3b7724629fb80b8b62f437015a43bb98dc3695d6
envyless <encyless@gmail.com>
2021-11-25 16:49:00 +0000

    hacker news study

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
     extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
+
+    // env가 추가가 안되면, browser인지 node인지 환경을 알 수 가 없으니 lint error가 발생한다.
+    env : {
+        "browser": true,
+        "node": true
+    }
 }
